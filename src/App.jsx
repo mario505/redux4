06363414125b537958ea231b37e5e17bc9f3d123
mario <mario_reyes505@hotmail.com>
2026-07-24 20:20:00 -0500

    add useEffect on the cart state on App.jsx, to send the cart data to a test firebase db

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Cart from "./components/Cart/Cart.jsx";
 import Layout from "./components/Layout/Layout.jsx";
 import Products from "./components/Shop/Products.jsx";
@@ -5,6 +6,14 @@ import { useSelector } from "react-redux";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://test-ba97c-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
