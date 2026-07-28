e48aa49e61dbb0a2a56b683539dee3970d27dca8
mario <mario_reyes505@hotmail.com>
2026-07-27 20:16:51 -0500

    create store/cart-actions.js to separate custom actions , added new fetching data actionn, added it to App.jsx to retrieve data when loading page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Products from "./components/Shop/Products.jsx";
 import { useSelector, useDispatch } from "react-redux";
 import { uiActions } from "./store/ui-slice.js";
 import Notification from "./components/UI/Notification.jsx";
-import { sendCartData } from "./store/cart-slice.js";
+import { sendCartData, fetchCartData } from "./store/cart-actions.js";
 
 let isInitial = true;
 
@@ -14,6 +14,10 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
   useEffect(() => {
     if (isInitial) {
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,13 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
   initialState: { items: [], totalQuantity: 0 },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       state.totalQuantity++;
+      if (!state.items) {
+        state.items = [];
+      }
       const existingItem = state.items.find((item) => item.id === newItem.id);
       if (!existingItem) {
         state.items.push({
@@ -35,51 +41,6 @@ const cartSlice = createSlice({
     },
   },
 });
-
-export const sendCartData = (cart) => {
-  return async (dispatch) => {
-    dispatch(
-      uiActions.showNotification({
-        status: "pending",
-        title: "Sending...",
-        message: "Sending cart data",
-      }),
-    );
-
-    const sendRequest = async () => {
-      const response = await fetch(
-        "https://test-ba97c-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        },
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-    };
-
-    try {
-      await sendRequest();
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success",
-          message: "Sent cart data succesfully!",
-        }),
-      );
-    } catch (error) {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: "Sending cart data failed",
-        }),
-      );
-    }
-  };
-};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
